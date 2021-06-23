b6eb59810a4c08b978d644ed976dc0475341278f
wadariel <84347029+wadariel@users.noreply.github.com>
2021-06-23 12:39:15 +0300

    Add files via upload

---
+++ GalacciaINT/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace GalacciaINT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Имя = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Цена = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Заказ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Телефон = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Адрес = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Дата = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Статус = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.dataGridView1.ColumnHeadersHeight = 35;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Имя,
+            this.Цена,
+            this.Заказ,
+            this.Телефон,
+            this.Адрес,
+            this.Дата,
+            this.Статус});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 200;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.Size = new System.Drawing.Size(952, 657);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Имя
+            // 
+            this.Имя.HeaderText = "Имя";
+            this.Имя.Name = "Имя";
+            // 
+            // Цена
+            // 
+            this.Цена.HeaderText = "Цена";
+            this.Цена.Name = "Цена";
+            // 
+            // Заказ
+            // 
+            this.Заказ.HeaderText = "Заказ";
+            this.Заказ.Name = "Заказ";
+            // 
+            // Телефон
+            // 
+            this.Телефон.HeaderText = "Телефон";
+            this.Телефон.Name = "Телефон";
+            // 
+            // Адрес
+            // 
+            this.Адрес.HeaderText = "Адрес";
+            this.Адрес.Name = "Адрес";
+            // 
+            // Дата
+            // 
+            this.Дата.HeaderText = "Дата";
+            this.Дата.Name = "Дата";
+            // 
+            // Статус
+            // 
+            this.Статус.HeaderText = "Статус";
+            this.Статус.Name = "Статус";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Coral;
+            this.button1.Location = new System.Drawing.Point(1082, 585);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 84);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Аналитика";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Coral;
+            this.button2.Location = new System.Drawing.Point(1082, 495);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 84);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Открыть таблицу";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Coral;
+            this.button3.Location = new System.Drawing.Point(1082, 405);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 84);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Обновить";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Coral;
+            this.button4.Location = new System.Drawing.Point(1082, 585);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 84);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Назад";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Name = "Заказы";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(952, 657);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            title1.BorderWidth = 3;
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            title1.ForeColor = System.Drawing.Color.SaddleBrown;
+            title1.Name = "Title1";
+            title1.Text = "Количество заказов в течение месяца";
+            this.chart1.Titles.Add(title1);
+            this.chart1.Visible = false;
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(12, 12);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series2.BorderWidth = 5;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Заказы";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(952, 657);
+            this.chart2.TabIndex = 6;
+            this.chart2.Text = "chart2";
+            title2.BorderWidth = 3;
+            title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            title2.ForeColor = System.Drawing.Color.SaddleBrown;
+            title2.Name = "Title1";
+            title2.Text = "Количество заказов в течение недели";
+            this.chart2.Titles.Add(title2);
+            this.chart2.Visible = false;
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Coral;
+            this.button5.Location = new System.Drawing.Point(443, 208);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(394, 79);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Месяц";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Coral;
+            this.button6.Location = new System.Drawing.Point(443, 356);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(394, 79);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Дни недели";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Coral;
+            this.button7.Location = new System.Drawing.Point(1082, 586);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(170, 83);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Назад";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BackgroundImage = global::GalacciaINT.Properties.Resources.f6fcb80111_2;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "Form1";
+            this.Text = "GalacciaApp";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Имя;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Цена;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Заказ;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Телефон;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Адрес;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Дата;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Статус;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ GalacciaINT/Form1.cs
@@ -0,0 +1,197 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace GalacciaINT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTableArgs();
+        }
+
+
+        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string ApplicationName = "Galaccia";
+
+        private void InitializeTableArgs()
+        {
+            // If modifying these scopes, delete your previously saved credentials
+            // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+            UserCredential credential;
+            using (var stream =
+            new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String spreadsheetId = "1t-enGK_DTcLygwNkjDs5mZ6B6b9IonqByeEHl03cCGQ";
+            String range = "A2:G";
+            SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1t-enGK_DTcLygwNkjDs5mZ6B6b9IonqByeEHl03cCGQ/edit
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
+
+            columnHeaderStyle.BackColor = System.Drawing.Color.Beige;
+            columnHeaderStyle.Font = new Font("Verdana", 15, FontStyle.Bold);
+            dataGridView1.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+
+
+            foreach (var row in values)
+            {   
+                dataGridView1.Rows.Add(row[0], row[1], row[2], row[3], row[4], row[5]);
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+      
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            dataGridView1.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/spreadsheets/d/1t-enGK_DTcLygwNkjDs5mZ6B6b9IonqByeEHl03cCGQ/edit#gid=1377732151");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            InitializeTableArgs();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            chart1.Visible = false;
+            chart2.Visible = false;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            chart1.Visible = true;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = true;
+
+
+            this.chart1.Series[0].Points.Clear();
+
+            for (int c = 0; c <= 30; c++)
+            {
+                int wd = 0;
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    string f = dataGridView1[5, i].Value.ToString();
+                    if (dataGridView1[5, i].Value.ToString().Substring(2, 3).Contains(","))
+                    {
+                        if (Convert.ToInt32(f.Substring(2, 2)) == c + 1) wd++;
+                    }
+                    else
+                    {
+                        if (Convert.ToInt32(f.Substring(2, 3)) == c + 1) wd++;
+                    }
+                }
+                this.chart1.Series[0].Points.AddXY(c + 1, wd);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            chart2.Visible = true;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = true;
+
+            this.chart2.Series[0].Points.Clear();
+           
+            for (int c = 0; c <= 6; c++)
+            {
+                int wd = 0;
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    string f = dataGridView1[5, i].Value.ToString();
+                    if (Convert.ToInt32(f.Substring(0, 1)) == c + 1) wd++;
+                }
+                this.chart2.Series[0].Points.AddXY(c + 1, wd);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            button5.Visible = true;
+            button6.Visible = true;
+            button7.Visible = false;
+            button4.Visible = true;
+            chart1.Visible = false;
+            chart2.Visible = false;
+        }
+    }
+}
+
